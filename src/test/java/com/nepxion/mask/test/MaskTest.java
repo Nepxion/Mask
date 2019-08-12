f0d5608ff74df18ce76f665c6b63f8be53560efb
Nepxion <1394997@qq.com>
2019-08-12 14:06:55 +0800

    重构代码

--- src/test/java/com/nepxion/mask/test/MaskTest.java
+++ src/test/java/com/nepxion/mask/test/MaskTest.java
@@ -14,11 +14,11 @@ import com.nepxion.mask.core.MaskType;
 public class MaskTest {
     public static void main(String[] args) throws Exception {
         System.out.println("手机号码 13812345678，脱敏值=" + MaskType.MOBILE.mask("13812345678"));
-        System.out.println("银行卡号 1234 1234 1234 1234 123，脱敏值=" + MaskType.BANK_CARD.mask("1234 1234 1234 1234 123"));
+        System.out.println("银行卡号 1234123412341234123，脱敏值=" + MaskType.BANK_CARD.mask("1234123412341234123"));
         System.out.println("身份证号 331012198001013030，脱敏值=" + MaskType.ID_CARD.mask("331012198001013030"));
         System.out.println("姓名 张三丰，脱敏值=" + MaskType.NAME.mask("张三丰"));
         System.out.println("信用卡CVV 123，脱敏值=" + MaskType.CVV.mask("123"));
-        System.out.println("信用卡有效期 12/20，脱敏值=" + MaskType.EXP.mask("12/20"));
+        System.out.println("信用卡有效期 1220，脱敏值=" + MaskType.EXP.mask("1220"));
         System.out.println("密码 1234567890，脱敏值=" + MaskType.PASSWORD.mask("1234567890"));
         System.out.println("验证码 1234，脱敏值=" + MaskType.CAPTCHA.mask("1234"));
         System.out.println("地址 上海市浦东新区浦建路1000弄1幢101室，脱敏值=" + MaskType.ADDRESS.mask("上海市浦东新区浦建路1000弄1幢101室"));
